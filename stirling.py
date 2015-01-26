23814b5d54d5c353e77294bc9319fdd04417e5eb
Joonatan Samuek <joonatan@ImaginaryComputer.(none)>
2015-01-26 20:40:13 +0200

    I am your new god little girls

--- stirling.py
+++ stirling.py
@@ -1,4 +1,7 @@
 from math import factorial, sqrt, pi, e
+# Using stirling approximation approximate factorial function. Very accurate after n > 1000
+# http://en.wikipedia.org/wiki/Stirling%27s_approximation
+
 
 def stirling(t):
 	return sqrt(2*pi*t)*(t/e)**t
